bac01d75352cbde9dd40e2c121ee03350b9f64be
Muhammad Munir <MuhammadShaMunir@gmail.com>
2019-06-12 22:18:58 -0500

    java program

---
+++ FizzBuzz.java
@@ -0,0 +1,9 @@
+public class FizzBuzz {
+    public String fizzBuzz(int number){
+    if (number % 3 == 0){
+    System.out.println("Fizz");
+    }
+    if (number % 5 == 0){
+    System.out.println("Buzz");
+    }
+}
